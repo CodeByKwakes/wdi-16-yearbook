3ae0e491ecdae94ada8288a2ccdd84f66b826490
KwakesProject <kwakes@me.com>
2015-11-13 11:46:25 +0000

    model schema set up

---
+++ model/student.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var studentSchema = mongoose.Schema({
+  name: String,
+  age: String,
+  year: String,
+  quote: String,
+  info: String,
+  photo: String
+});
+
+module.exports = mongoose("Student", studentSchema);
+
